13f230bb1f9b05bcb308151550e2e644ea001d80
Maks Rozman <maksrozman.mr@gmail.com>
2025-11-18 17:51:46 +0100

    basic stats

--- simulations.py
+++ simulations.py
@@ -1,3 +1,30 @@
-#hlkfasdhflk
-def ena(x):
-    print(x)
+from bacc import build_shoe, play_bacc
+
+shoe = build_shoe()
+
+hands_number = 100000
+
+banker_win = 0
+player_win = 0
+tie = 0
+
+
+for i in range(hands_number):
+    result = play_bacc(shoe)
+    if result == "Player":
+        player_win += 1
+    elif result == "Banker":
+        banker_win += 1
+    else:
+        tie += 1
+
+
+
+print("Banker wins", banker_win)
+print("Player wins", player_win)
+print("Ties", tie)
+print()
+print("Percentages")
+print("Banker", banker_win / hands_number * 100, "%")
+print("Player", player_win / hands_number * 100, "%")
+print("Tie", tie / hands_number * 100, "%")
