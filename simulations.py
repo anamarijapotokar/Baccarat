1bf6c84f5b390cba7ec90bef2565269aff6ef222
Maks Rozman <maksrozman.mr@gmail.com>
2025-11-18 18:08:06 +0100

    plot

--- simulations.py
+++ simulations.py
@@ -1,15 +1,19 @@
 from bacc import build_shoe, play_bacc
+import matplotlib.pyplot as plt
 
 shoe = build_shoe()
 
-hands_number = 100000
+hands_number = 10000
 
 banker_win = 0
 player_win = 0
 tie = 0
 
+banker_share = []
+player_share = []
+tie_share = []
 
-for i in range(hands_number):
+for i in range(1, hands_number + 1):
     result = play_bacc(shoe)
     if result == "Player":
         player_win += 1
@@ -17,6 +21,10 @@ for i in range(hands_number):
         banker_win += 1
     else:
         tie += 1
+    
+    banker_share.append(banker_win / i)
+    player_share.append(player_win / i)
+    tie_share.append(tie / i)
 
 
 
@@ -28,3 +36,13 @@ print("Percentages")
 print("Banker", banker_win / hands_number * 100, "%")
 print("Player", player_win / hands_number * 100, "%")
 print("Tie", tie / hands_number * 100, "%")
+
+
+plt.plot(banker_share, label="Banker %")
+plt.plot(player_share, label="Player %")
+plt.plot(tie_share, label="Tie %")
+
+plt.xlabel("Hands played")
+plt.ylabel("Win %")
+plt.title("Share of wins")
+plt.show()
