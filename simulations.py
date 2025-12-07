93132289322a2da24c886d5aea929c86227d8fd9
anamarijapotokar <anamaria.potokar01@gmail.com>
2025-12-07 13:29:24 +0100

    calculating house edge; neki ne štima

--- simulations.py
+++ simulations.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 shoe = build_shoe()
 
-hands_number = 10000
+hands_number = 1000000
 
 banker_win = 0
 player_win = 0
@@ -26,13 +26,18 @@ for i in range(1, hands_number + 1):
         tie += 1
 
 # We calculate the share of wins for each hand.
-banker_share = banker_win/hands_number
-player_share = player_win/hands_number
-tie_share = tie/hands_number
+banker_share = banker_win / hands_number
+player_share = player_win / hands_number
+tie_share = tie / hands_number
 
 # We calcluate the house edge for each hand.
-player_ev = (player_win * 1 + banker_win * (-1) + tie * 0) / hands_number
+player_ev = (player_win * 1 + banker_win * (-1) + tie * (-1)) / hands_number
+banker_ev = (player_win * (-1) + banker_win * 0.95 + tie * (-1)) / hands_number
+tie_ev = (tie * 8 + (hands_number - tie) * (-1)) / hands_number
 
+player_house_edge = -player_ev
+banker_house_edge = -banker_ev
+tie_house_edge    = -tie_ev
 
 print("Banker wins:", banker_win)
 print("Player wins:", player_win)
@@ -42,3 +47,12 @@ print("Percentages")
 print("Banker:", banker_share * 100, "%")
 print("Player:", player_share * 100, "%")
 print("Tie:", tie_share * 100, "%")
+print()
+print("EV per hand (Player bet):", player_ev)
+print("EV per hand (Banker bet):", banker_ev)
+print("EV per hand (Tie bet):", tie_ev)
+print()
+print("House edge estimates:")
+print("Player bet:",  player_house_edge * 100, "%")
+print("Banker bet:",  banker_house_edge * 100, "%")
+print("Tie bet:   ",  tie_house_edge * 100, "%")
